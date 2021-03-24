6d7098ebc7529aadecf7a3226d43ea9b4eb16047
Gabriele <gabboxl0@gmail.com>
2021-03-24 21:45:47 +0100

    fortunalteub: changes for lineage-18.x * Remove non-lineage makefiles * Switch from add_lunch_combo to COMMON_LUNCH_CHOICES * Use msm8916-common RAM props
    Change-Id: I9c332ef420f8fb5b3382031ce41641f375a7fcf4

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := device/samsung/fortunalteub
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/rr_fortunalteub.mk \
-	$(LOCAL_DIR)/pa_fortunalteub.mk \
 	$(LOCAL_DIR)/lineage_fortunalteub.mk
+
+COMMON_LUNCH_CHOICES := \
+	lineage_fortunalteub-eng \
+	lineage_fortunalteub-userdebug \
+	lineage_fortunalteub-user
